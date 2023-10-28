a2bccafb686712f7ad2e0704a72243a27960426e
Henry Timms <36480544+Hen676@users.noreply.github.com>
2023-10-28 16:49:51 +0100

    2nd Init

--- js/main.js
+++ js/main.js
@@ -1,37 +1,96 @@
 // Global Vars
 /** Adds extra logging. */
 const  debug = true;
-/** Debug fakes the local storage to be empty. */
 const  debugFakeEmptyStorage = false;
-/** Is the stop watch running. */
+
+// Stopwatch vars
 let stopwatchRunning = false;
-/** Time value in milliseconds. */
-let stopwatchTime;
+let stopwatchStartTime = 0;
+let stopwatchElapsedTime = 0;
+var worker;
+
 /** Array of TimeTask's. */
 let taskStorage = [];
 
 // HTML Elements
-let taskForm = document.getElementById("createTimeTask");
-let stopwatch = document.getElementById("stopwatch");
-let totalTime = document.getElementById("totalTime");
-let totalTasks = document.getElementById("totalTasks");
-let taskList = document.getElementById("taskList");
+let body = document.getElementById("body");
 
-stopwatch.addEventListener("click", (e) => {
-    if(stopwatchRunning) {
-        stopwatchTime = new Date().getTime() - stopwatchTime;
-        stopwatch.value = "Restart"
+let taskFormForm = document.getElementById("createTimeTask");
+
+let darkMode = document.getElementById("darkMode");
+let darkModeIcon = document.getElementById("darkModeIcon");
+
+let startA = document.getElementById("start");
+let pauseA = document.getElementById("pause");
+let restartA = document.getElementById("restart");
+
+let submitA = document.getElementById("submit");
+let stopWatchTimeP = document.getElementById("stopWatchTime");
+
+let totalTimeP = document.getElementById("totalTime");
+let totalTasksP = document.getElementById("totalTasks");
+let taskListSection = document.getElementById("taskList");
+
+// Stopwatch events and functions
+startA.addEventListener("click", (e) => {
+    if(!stopwatchRunning) {
+        stopwatchRunning = true;
+        stopwatchStartTime = Date.now() - stopwatchElapsedTime;
+        worker = setInterval(updateTime, 75);
     }
-    else {
-        stopwatchTime = new Date().getTime();
-        stopwatch.value = "Stop"
-    }
-    stopwatchRunning = !stopwatchRunning;
 });
 
-taskForm.addEventListener("submit", (e) => {
-    e.preventDefault();
+pauseA.addEventListener("click", (e) => {
+    if(stopwatchRunning) {
+        stopwatchRunning = false;
+        stopwatchElapsedTime = Date.now() - stopwatchStartTime;
+        clearInterval(worker);
+    }
+});
 
+restartA.addEventListener("click", (e) => {
+    if(stopwatchRunning)
+        stopwatchRunning = false;
+    stopwatchStartTime = 0;
+    stopwatchElapsedTime = 0;
+    updateTime();
+    clearInterval(worker);
+});
+
+darkMode.addEventListener("click", (e) => {
+    body.classList.toggle("dark");
+
+    var darkModeBool = body.classList.contains("dark");
+    toggleDarkModeIcon(darkModeBool);
+
+    if (isStorageAvailable)
+        localStorage.setItem("dark", darkModeBool);
+});
+
+
+
+// Dark mode
+var isStorageAvailable = typeof Storage !== "undefined";
+if (isStorageAvailable) {
+    if (localStorage.getItem("dark") === "true") {
+        body.classList.add("dark");
+        toggleDarkModeIcon();
+    }
+}
+function toggleDarkModeIcon() {
+    darkModeIcon.classList.toggle("fa-moon");
+    darkModeIcon.classList.toggle("fa-sun");
+}
+
+
+function updateTime() {
+    if(stopwatchRunning)
+        stopwatchElapsedTime = Date.now() - stopwatchStartTime;
+    stopWatchTimeP.innerHTML = formatTime(stopwatchElapsedTime);
+}
+
+// Task creation events
+submitA.addEventListener("click", (e) => {
     if(debug)
         console.log(e);
 
@@ -43,19 +102,29 @@ taskForm.addEventListener("submit", (e) => {
         tasks.value = 1;
 
     // Send alert anr return if stop watch is running or has never run.
-    if(stopwatchRunning || stopwatchTime == null) {
+    if(stopwatchRunning || stopwatchElapsedTime == 0) {
         alert("No time given, please give a time")
         return;
     }
     var timeTask;
-    if(taskTitle.value == "")
-        timeTask = new TimeTask("Default title", parseInt(tasks.value), parseInt(stopwatchTime), new Date().toDateString());
-    else
-        timeTask = new TimeTask(taskTitle.value, parseInt(tasks.value), parseInt(stopwatchTime), new Date().toDateString());
+    if(taskTitle.value == "") {
+        timeTask = new TimeTask(
+            "Default title", 
+            parseInt(tasks.value), 
+            parseInt(stopwatchElapsedTime), 
+            new Date().getTime());
+    } else {
+        timeTask = new TimeTask(
+            taskTitle.value, 
+            parseInt(tasks.value), 
+            parseInt(stopwatchElapsedTime), 
+            new Date().getTime());
+    }
 
 
-    if(debug)
+    if(debug) {
         console.log(timeTask);
+    }
 
     // Save to local storage
     taskStorage.push(timeTask);
@@ -64,9 +133,8 @@ taskForm.addEventListener("submit", (e) => {
     // Update totals
     updateTotals();
 
-    stopwatch.value = "Start";
-    stopwatchTime = 0;
-    taskForm.reset();
+    stopwatchTimeTotal = 0;
+    taskFormForm.reset();
 });
 
 /** Get TimeTasks from local storage. 
@@ -88,15 +156,15 @@ function getLocalStorageOrEmpty() {
  * @returns {string} Time formatted into a string
  */
 function formatTime(time) {
-    const seconds = Math.floor((time / 1000) % 60);
-    const minutes = Math.floor((time / 1000 / 60) % 60);
-    const hours = Math.floor((time / 1000 / 60 / 60) % 24);
+    function padAndFloor(unit) {
+        return ('0' + Math.floor(unit)).slice(-2);
+    }
 
-    if(hours == 0 && minutes == 0)
-        return seconds.toString() + "s";
-    if(hours == 0)
-        return [minutes.toString() + "m",seconds.toString() + "s"].join(" ");
-    return [hours.toString() + "h",minutes.toString() + "m",seconds.toString() + "s"].join(" ");
+    const seconds = padAndFloor((time / 1000) % 60);
+    const minutes = padAndFloor((time / 1000 / 60) % 60);
+    const hours = padAndFloor((time / 1000 / 60 / 60) % 24);
+
+    return `${hours}:${minutes}:${seconds}`;
 }
 
 /** Update running totals. */
@@ -104,35 +172,74 @@ function updateTotals() {
     var currentDate = new Date().toDateString();
     var time = 0;
     var tasks = 0;
-
     taskStorage.forEach(element => {
-        if(element.date == currentDate) {
+        if(new Date(element.date).toDateString() == currentDate) {
             time += parseInt(element.time);
             tasks += parseInt(element.tasks)
-        }
-        if(debug) {
-            console.log(element.date == currentDate)
         }
     });
 
     // Total tasks
-    totalTasks.innerText = tasks;
-    totalTime.innerText = formatTime(time);
+    totalTasksP.innerText = tasks;
+    totalTimeP.innerText = formatTime(time);
+    if (debug) {
+        console.log(`Total tasks: ${tasks}`);
+        console.log(`Total time: ${time}`);
+    }
+    loadTasks();
+}
+
+function deleteTask(index) {
+    if (debug)
+        console.log("Deleting: " + index);
+
+    taskStorage.splice(index, 1);
+    localStorage.Tasks = JSON.stringify(taskStorage);
+
+    loadTasks();
 }
 
 /** Initialize all data need for the website + fill in empty values. */
 function load() {
+    function compareFn(a, b) {
+        if (a.date < b.date) {
+            return -1;
+        } else if (a.date > b.date) {
+            return 1;
+        }
+        return 0;
+    }
+
+
     taskStorage = getLocalStorageOrEmpty();
     updateTotals();
 
+    taskStorage.sort(compareFn)
+
     // Display the last 10 tasks
-    taskList.innerHTML = taskStorage.reverse().slice(0, 10).map(i => `
+    loadTasks();
+}
+
+function loadTasks() {
+    taskListSection.innerHTML = taskStorage.map((element, index) => `
     <div class="task">
-        <p>${i.title}</p>
-        <p>Date: ${i.date}</p>
-        <p>Time taken: ${formatTime(i.time)}</p>
-        <p>Tasks: ${i.tasks}</p>
-        <p>Time taken per task: ${formatTime(i.time / i.tasks)}</p>
+        <div class="top">
+            <h1>${element.title}</h1>
+            <p>${new Date(element.date).toDateString()}</p>
+        </div>
+        <ul class="iconList">
+            <li class="tooltip">
+                <a class="iconSimple" onclick="deleteTask(${index})">
+                    <i class="fa-solid fa-xmark fa-xl"></i>
+                </a>
+                <span class="tooltipText">Delete</span>
+            </li>
+        </ul>
+        <div class="bottom">
+            <p>Time: ${formatTime(element.time)}</p>
+            <p>Tasks: ${element.tasks}</p>
+            <p>Time per task: ${formatTime(element.time / element.tasks)}</p>
+        </div>
     </div>
     `).join('');
 }
--- js/timeTask.js
+++ js/timeTask.js
@@ -1,4 +1,10 @@
 class TimeTask {
+    /** Get TimeTasks from local storage. 
+     * @param {string} title - Title of task.
+     * @param {number} tasks - Amount of tasks.
+     * @param {number} time - Time in milliseconds.
+     * @param {number} date - Date in milliseconds.
+     * */
     constructor(title, tasks, time, date) {
         this.title = title;
         this.tasks = tasks;
