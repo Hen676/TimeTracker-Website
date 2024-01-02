42ff09c63cf6ffe091f9f95312952124090ba0bf
Henry Timms <36480544+Hen676@users.noreply.github.com>
2024-01-02 22:46:40 +0000

    Settings menu overhaul + UUID for tasks

--- js/main.js
+++ js/main.js
@@ -4,18 +4,14 @@ let stopwatchRunning = false;
 let stopwatchStartTime = 0;
 /** @type {number} */
 let stopwatchElapsedTime = 0;
+
 /** Worker for the stopwatch. @type {number} */
 var workerId;
 
-/** @type {Settings} */
-let settings = getSettings();
-/** @type  {TimeTask[]} */
-let taskStorage = getTasks();
-
 // Check storage and settings
 if (settings.debug) {
-    console.log(taskStorage);
-    console.log(settings);
+	console.log(taskStorage);
+	console.log(settings);
 }
 
 // HTML Elements
@@ -35,95 +31,91 @@ let avgTimePerTaskP = document.getElementById("avgTimePerTask");
 let taskListSection = document.getElementById("taskList");
 /** @type  {HTMLElement} */
 let settingsAside = document.getElementById("settings");
+/** @type  {HTMLElement} */
+let taskTitle = document.getElementById("taskTitle");
+/** @type  {HTMLElement} */
+let tasks = document.getElementById("tasks");
 
 /** @type  {HTMLCollection<HTMLButtonElement>} */
 let stopwatchControls = document.getElementsByClassName("controls");
-
 
 /*
     Stopwatch
 */
 function updateTime() {
-    if(stopwatchRunning) 
-        stopwatchElapsedTime = Date.now() - stopwatchStartTime;
-    stopWatchTimeP.innerHTML = formatTime(stopwatchElapsedTime);
+	if (stopwatchRunning) stopwatchElapsedTime = Date.now() - stopwatchStartTime;
+	stopWatchTimeP.innerHTML = formatTime(stopwatchElapsedTime);
 }
 
-/** 
+/**
  * Stopwatch cannot be running while editing the time.
  * Min time of 0.
  * Max time of 23:59:59.
  * @param {number} time - Time in milliseconds.
  */
 function changeTimeInMilliseconds(time) {
-    if (stopwatchRunning) return;
-    stopwatchElapsedTime += time;
-    if (stopwatchElapsedTime < 0) 
-        stopwatchElapsedTime = 0;
-    else if (stopwatchElapsedTime > 86_399_999) 
-        stopwatchElapsedTime = 86_399_999;
-    stopWatchTimeP.innerHTML = formatTime(stopwatchElapsedTime);
+	if (stopwatchRunning) return;
+	stopwatchElapsedTime += time;
+	if (stopwatchElapsedTime < 0) stopwatchElapsedTime = 0;
+	else if (stopwatchElapsedTime > 86_399_999) stopwatchElapsedTime = 86_399_999;
+	stopWatchTimeP.innerHTML = formatTime(stopwatchElapsedTime);
 }
-
 
 /** Update running totals and show today's tasks */
 function updateTotalsAndShowTasks() {
-    var currentDate = new Date().toDateString();
-    var time = 0;
-    var tasks = 0;
-    taskStorage.forEach(element => {
-        if(new Date(element.date).toDateString() == currentDate) {
-            time += parseInt(element.time);
-            tasks += parseInt(element.tasks)
-        }
-    });
+	var currentDate = new Date().toDateString();
+	var time = 0;
+	var tasks = 0;
+	taskStorage.forEach((element) => {
+		if (new Date(element.date).toDateString() == currentDate) {
+			time += parseInt(element.time);
+			tasks += parseInt(element.tasks);
+		}
+	});
 
-    // Total tasks
-    totalTasksP.innerText = tasks;
-    totalTimeP.innerText = formatTime(time);
-    avgTimePerTaskP.innerText = formatTime(time/tasks)
-
-    showTasks();
-}
-
-function deleteTask(index) {
-    if (settings.debug) console.log("Deleting: " + index);
-    taskStorage.splice(index, 1);
-    localStorage.Tasks = JSON.stringify(taskStorage);
-    updateTotalsAndShowTasks();
+	// Total tasks
+	totalTasksP.innerText = tasks;
+	totalTimeP.innerText = formatTime(time);
+	if (tasks != 0) avgTimePerTaskP.innerText = formatTime(time / tasks);
+	else avgTimePerTaskP.innerText = formatTime(time);
+	showTasks();
 }
 
 /**
  * Adds click event that toggles "visible" class
- * @param {HTMLElement} element 
+ * @param {HTMLElement} element
  */
 function addEventListenerToggleSettings(element) {
-    if (element != null)
-        element.addEventListener("click", () => toggleVisibleClass());
+	if (element != null)
+		element.addEventListener("click", () => toggleVisibleClass());
 }
 
 function toggleVisibleClass() {
-    settingsAside.classList.toggle("visible");
+	settingsAside.classList.toggle("visible");
 }
 
 function showTasks() {
-    taskStorage.sort((a, b) => {
-        if (a.date > b.date) return -1;
-        else if (a.date < b.date) return 1;
-        return 0;
-    });
+	taskStorage.sort((a, b) => {
+		if (a.date > b.date) return -1;
+		else if (a.date < b.date) return 1;
+		return 0;
+	});
 
-    taskListSection.innerHTML = getTodaysTasks().map((element, index) => `
-    <div class="task">
+	taskListSection.innerHTML = getTodaysTasks()
+		.map(
+			(element, index) => `
+    <li class="task">
         <ul class="iconList delete">
             <li class="tooltip">
-                <a class="iconSimple" onclick="copyTimePerTaskToClipboard(${index})">
+                <a class="iconSimple" onclick="copyTimePerTaskToClipboard('${
+									element.id
+								}')">
                     <i class="fa-solid fa-copy fa-xl"></i>
                 </a>
                 <span class="tooltipText">Copy Time Per Task</span>
             </li>
             <li class="tooltip">
-                <a class="iconSimple" onclick="deleteTask(${index})">
+                <a class="iconSimple" onclick="deleteTask('${element.id}')">
                     <i class="fa-solid fa-xmark fa-xl"></i>
                 </a>
                 <span class="tooltipText">Delete</span>
@@ -142,134 +134,153 @@ function showTasks() {
         <div class="date">
             <p>${formatTime(element.date)}</p>
         </div>
-    </div>
-    `).join('');
+    </li>
+    `
+		)
+		.join("");
 }
 
-function copyTimePerTaskToClipboard(index) {
-    if (settings.debug) console.log("Copying time per task: " + index);
-    var text = formatTimeForCopying(taskStorage[index].time / taskStorage[index].tasks);
-    navigator.clipboard.writeText(text).then(()=>{
-        console.log('copied')
-    },
-    ()=>{
-        console.log('failed to copy')
-    })
+function copyTimePerTaskToClipboard(uuid) {
+	if (settings.debug) console.log("Copying time per task: " + uuid);
+	timeTask = findTask(uuid);
+	var text = formatTimeForCopying(timeTask.time / timeTask.tasks);
+	navigator.clipboard.writeText(text).then(
+		() => {
+			console.log("copied");
+		},
+		() => {
+			console.log("failed to copy");
+		}
+	);
+}
+
+function deleteTask(uuid) {
+	if (settings.debug) console.log("Deleting: " + uuid);
+	taskStorage.splice(
+		taskStorage.findIndex((timeTask) => timeTask.id == uuid),
+		1
+	);
+	localStorage.Tasks = JSON.stringify(taskStorage);
+	updateTotalsAndShowTasks();
+}
+
+function findTask(uuid) {
+	return taskStorage.find((timeTask) => timeTask.id == uuid);
 }
 
 function getTodaysTasks() {
-    var currentDate = new Date().toDateString();
-    return taskStorage.filter(element => {
-        return new Date(element.date).toDateString() == currentDate;
-    });
+	var currentDate = new Date().toDateString();
+	return taskStorage.filter((element) => {
+		return new Date(element.date).toDateString() == currentDate;
+	});
 }
 
-/** 
+function toggleStopwatchPlusMinusControls() {
+	for (let button of stopwatchControls) {
+		button.classList.toggle("disabled");
+	}
+}
+
+function start() {
+	if (!stopwatchRunning) {
+		stopwatchRunning = true;
+		toggleStopwatchPlusMinusControls();
+		stopwatchStartTime = Date.now() - stopwatchElapsedTime;
+		workerId = setInterval(updateTime, 75);
+	}
+}
+
+function pause() {
+	if (stopwatchRunning) {
+		stopwatchRunning = false;
+		toggleStopwatchPlusMinusControls();
+		stopwatchElapsedTime = Date.now() - stopwatchStartTime;
+		clearInterval(workerId);
+	}
+}
+
+function resetForm() {
+	if (stopwatchRunning) {
+		stopwatchRunning = false;
+		toggleStopwatchPlusMinusControls();
+	}
+	stopwatchStartTime = 0;
+	stopwatchElapsedTime = 0;
+	tasks.value = settings.defaultTasks;
+	taskTitle.value = "";
+	updateTime();
+	clearInterval(workerId);
+}
+
+function submit() {
+	// Send alert anr return if stop watch is running or has never run.
+	if (stopwatchRunning || stopwatchElapsedTime == 0) {
+		alert("No time given, please give a time");
+		return;
+	}
+	var timeTask;
+	if (taskTitle.value == "") {
+		timeTask = new TimeTask(
+			settings.defaultTitle,
+			parseInt(tasks.value),
+			parseInt(stopwatchElapsedTime),
+			new Date().getTime()
+		);
+	} else {
+		timeTask = new TimeTask(
+			taskTitle.value,
+			parseInt(tasks.value),
+			parseInt(stopwatchElapsedTime),
+			new Date().getTime()
+		);
+	}
+	if (settings.debug) console.log(timeTask);
+	// Save to local storage
+	taskStorage.push(timeTask);
+	localStorage.Tasks = JSON.stringify(taskStorage);
+	// Update totals
+	updateTotalsAndShowTasks();
+	// Reset form
+	resetForm();
+}
+
+/**
  *  Initialize all data need for the website + fill in empty values.
  *  Also initializes all events.
  */
 function load() {
-    // Register stopwatch events
-    document.getElementById("start").addEventListener("click", () => {
-        if(!stopwatchRunning) {
-            stopwatchRunning = true;
-            toggleStopwatchPlusMinusControls();
-            stopwatchStartTime = Date.now() - stopwatchElapsedTime;
-            workerId = setInterval(updateTime, 75);
-        }
-    });
-    document.getElementById("pause").addEventListener("click", () => {
-        if(stopwatchRunning) {
-            stopwatchRunning = false;
-            toggleStopwatchPlusMinusControls();
-            stopwatchElapsedTime = Date.now() - stopwatchStartTime;
-            clearInterval(workerId);
-        }
-    });
-    document.getElementById("reset").addEventListener("click", resetForm);
+	// Register settings buttons
+	addEventListenerToggleSettings(document.getElementById("settingsToggle"));
+	addEventListenerToggleSettings(document.getElementById("closeSettings"));
+	// Esc key opens settings
+	window.onkeydown = function (event) {
+		if (event.keyCode == 27) toggleVisibleClass();
+	};
 
-    function resetForm() {
-        if(stopwatchRunning) {
-            stopwatchRunning = false;
-            toggleStopwatchPlusMinusControls();
-        }
-        stopwatchStartTime = 0;
-        stopwatchElapsedTime = 0;
-        tasks.value = settings.defaultTasks;
-        updateTime();
-        clearInterval(workerId);
-    }
+	document.getElementById("saveSettings").addEventListener("click", () => {
+		settings.defaultTitle = document.getElementById("defaultTaskTitle").value;
+		settings.defaultTasks = parseInt(
+			document.getElementById("defaultTasks").value
+		);
+		settings.debug = document.getElementById("debug").checked;
+		settings.debugStorage = document.getElementById("debugStorage").checked;
+		saveSettings();
+	});
 
-    function toggleStopwatchPlusMinusControls() {
-        for(let button of stopwatchControls) {
-            button.classList.toggle("disabled");
-        };
-    }
+	document.getElementById("resetSettings").addEventListener("click", () => {
+		settings = defaultSettings();
+		saveSettings();
+		showSettings();
+	});
 
-    // Register task submit button
-    document.getElementById("submit").addEventListener("click", () => {
-        let taskTitle = document.getElementById("taskTitle");
-        let tasks = document.getElementById("tasks");
+	let tasks = document.getElementById("tasks");
+	tasks.value = settings.defaultTasks;
+	taskTitle.placeholder = settings.defaultTitle;
 
-        // Send alert anr return if stop watch is running or has never run.
-        if(stopwatchRunning || stopwatchElapsedTime == 0) {
-            alert("No time given, please give a time")
-            return;
-        }
-        var timeTask;
-        if(taskTitle.value == "") {
-            timeTask = new TimeTask(
-                settings.defaultTitle, 
-                parseInt(tasks.value), 
-                parseInt(stopwatchElapsedTime), 
-                new Date().getTime());
-        } else {
-            timeTask = new TimeTask(
-                taskTitle.value, 
-                parseInt(tasks.value), 
-                parseInt(stopwatchElapsedTime), 
-                new Date().getTime());
-        }
-        if(settings.debug) console.log(timeTask);
-        // Save to local storage
-        taskStorage.push(timeTask);
-        localStorage.Tasks = JSON.stringify(taskStorage);
-        // Update totals
-        updateTotalsAndShowTasks();
-        // Reset form
-        resetForm();
-    });
-
-    // Register settings buttons
-    addEventListenerToggleSettings(document.getElementById("settingsToggle"));
-    addEventListenerToggleSettings(document.getElementById("closeSettings"));
-    // Esc key opens settings
-    window.onkeydown = function (event) {
-        if (event.keyCode == 27) toggleVisibleClass();
-    };
-
-    document.getElementById("saveSettings").addEventListener("click", () => {
-        settings.defaultTitle = document.getElementById("defaultTaskTitle").value;
-        settings.defaultTasks = parseInt(document.getElementById("defaultTasks").value);
-        settings.debug = document.getElementById("debug").checked;
-        settings.debugStorage = document.getElementById("debugStorage").checked;
-        saveSettings();
-    });
-
-    document.getElementById("resetSettings").addEventListener("click", () => {
-        settings = defaultSettings();
-        saveSettings();
-        showSettings();
-    });
-
-    let tasks = document.getElementById("tasks");
-    tasks.value = settings.defaultTasks;
-    taskTitle.placeholder = settings.defaultTitle;
-
-    // Update totals and show tasks
-    updateTotalsAndShowTasks();
-    // Show settings
-    showSettings();
+	// Update totals and show tasks
+	updateTotalsAndShowTasks();
+	// Show settings
+	showSettings();
 }
 
 // Call load on window load
--- js/util.js
+++ js/util.js
@@ -1,31 +1,39 @@
+/** @type {number} */
+let version = 1.1;
+/** @type {Settings} */
+let settings = getSettings();
+/** @type  {TimeTask[]} */
+let taskStorage = getTasks();
+
 class TimeTask {
-    /** Get TimeTasks from local storage. 
-     * @param {string} title - Title of task.
-     * @param {number} tasks - Amount of tasks.
-     * @param {number} time - Time in milliseconds.
-     * @param {number} date - Date in milliseconds.
-     * */
-    constructor(title, tasks, time, date) {
-        this.title = title;
-        this.tasks = tasks;
-        this.time = time;
-        this.date = date;
-    }
+	/** Get TimeTasks from local storage.
+	 * @param {string} title - Title of task.
+	 * @param {number} tasks - Amount of tasks.
+	 * @param {number} time - Time in milliseconds.
+	 * @param {number} date - Date in milliseconds.
+	 * */
+	constructor(title, tasks, time, date) {
+		this.title = title;
+		this.tasks = tasks;
+		this.time = time;
+		this.date = date;
+		this.id = getID();
+	}
 }
 
 class Settings {
-    /** Get Settings from local storage. 
-     * @param {boolean} debug - Debug mode.
-     * @param {boolean} debugStorage - Fakes empty storage.
-     * @param {string} defaultTitle - Default task title.
-     * @param {number} defaultTasks - Default amount of tasks.
-     * */
-    constructor(debug, debugStorage, defaultTitle, defaultTasks) {
-        this.debug = debug;
-        this.debugStorage = debugStorage;
-        this.defaultTitle = defaultTitle;
-        this.defaultTasks = defaultTasks;
-    }
+	/** Get Settings from local storage.
+	 * @param {boolean} debug - Debug mode.
+	 * @param {boolean} debugStorage - Fakes empty storage.
+	 * @param {string} defaultTitle - Default task title.
+	 * @param {number} defaultTasks - Default amount of tasks.
+	 * */
+	constructor(debug, debugStorage, defaultTitle, defaultTasks) {
+		this.debug = debug;
+		this.debugStorage = debugStorage;
+		this.defaultTitle = defaultTitle;
+		this.defaultTasks = defaultTasks;
+	}
 }
 
 /**
@@ -34,13 +42,15 @@ class Settings {
  * @returns {string} Time formatted into a string
  */
 function formatTime(time) {
-    // TODO:: Add cache on padAndFloor?
-    function padAndFloor(unit) { return ('0' + Math.floor(unit)).slice(-2); }
+	// TODO:: Add cache on padAndFloor?
+	function padAndFloor(unit) {
+		return ("0" + Math.floor(unit)).slice(-2);
+	}
 
-    let seconds = padAndFloor((time / 1000) % 60);
-    let minutes = padAndFloor((time / 1000 / 60) % 60);
-    let hours = padAndFloor((time / 1000 / 60 / 60) % 24);
-    return `${hours}:${minutes}:${seconds}`;
+	let seconds = padAndFloor((time / 1000) % 60);
+	let minutes = padAndFloor((time / 1000 / 60) % 60);
+	let hours = padAndFloor((time / 1000 / 60 / 60) % 24);
+	return `${hours}:${minutes}:${seconds}`;
 }
 
 /**
@@ -49,64 +59,75 @@ function formatTime(time) {
  * @returns {string} Time formatted into a string
  */
 function formatTimeForCopying(time) {
-    if(time < 1000)
-        return "0";
+	if (time < 1000) return "0";
 
-    let seconds = Math.floor((time / 1000) % 60);
-    let minutes = Math.floor((time / 1000 / 60) % 60);
-    let hours = Math.floor((time / 1000 / 60 / 60) % 24);
+	let seconds = Math.floor((time / 1000) % 60);
+	let minutes = Math.floor((time / 1000 / 60) % 60);
+	let hours = Math.floor((time / 1000 / 60 / 60) % 24);
 
-    var text = "";
-    if (hours != 0)
-        text += `${hours}h `
-    if (minutes != 0)
-        text += `${minutes}m `
-    if (seconds != 0)
-        text += `${seconds}s `
-    return text.slice(0, -1);
+	var text = "";
+	if (hours != 0) text += `${hours}h `;
+	if (minutes != 0) text += `${minutes}m `;
+	if (seconds != 0) text += `${seconds}s `;
+	return text.slice(0, -1);
 }
 
 /*
     Local storage functions
 */
 
-/** 
- * Gets TimeTasks from local storage. 
+/**
+ * Gets TimeTasks from local storage.
  * @returns {TimeTask[] | []} TimeTasks from local storage
-*/
+ */
 function getTasks() {
-    if(localStorage.Tasks == null || settings.debugStorage) return [];
-    return JSON.parse(localStorage.Tasks) ?? [];
+	if (localStorage.Tasks == null || settings.debugStorage) return [];
+	return JSON.parse(localStorage.Tasks) ?? [];
 }
 
 function saveTasks() {
-    localStorage.Tasks = JSON.stringify(taskStorage);
+	localStorage.Tasks = JSON.stringify(taskStorage);
 }
 
-/** 
- * Gets Settings from local storage. 
+/**
+ * Gets Settings from local storage.
  * @returns {Settings} Settings from local storage
-*/
+ */
 function getSettings() {
-    if(localStorage.Settings == null) return defaultSettings();
-    return JSON.parse(localStorage.Settings) ?? defaultSettings();
+	if (localStorage.Settings == null) return defaultSettings();
+	return JSON.parse(localStorage.Settings) ?? defaultSettings();
 }
 
-/** 
+/**
  * Gets default Settings
  * @returns {Settings}
-*/
+ */
 function defaultSettings() {
-    return new Settings(false, false, "Default Title", 1);
+	return new Settings(false, false, "Default Title", 1);
 }
 
 function saveSettings() {
-    localStorage.Settings = JSON.stringify(settings);
+	localStorage.Settings = JSON.stringify(settings);
 }
 
 function showSettings() {
-    document.getElementById("defaultTaskTitle").value = settings.defaultTitle;
-    document.getElementById("defaultTasks").value = settings.defaultTasks;
-    document.getElementById("debug").checked = settings.debug ;
-    document.getElementById("debugStorage").checked = settings.debugStorage;
+	document.getElementById("defaultTaskTitle").value = settings.defaultTitle;
+	document.getElementById("defaultTasks").value = settings.defaultTasks;
+	document.getElementById("debug").checked = settings.debug;
+	document.getElementById("debugStorage").checked = settings.debugStorage;
+}
+
+function getID() {
+	let uuid = self.crypto.randomUUID();
+	while (taskStorage.find((timeTask) => timeTask.id == uuid) != undefined)
+		uuid = self.crypto.randomUUID();
+	return uuid;
+}
+
+if (localStorage.version == undefined) {
+	taskStorage.forEach((item) => {
+		item.id = getID();
+	});
+	localStorage.version = JSON.stringify(version);
+	saveTasks();
 }
